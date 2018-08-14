0709d5c36a79bcabba896bad6cfdec491511f12f
Margo Schaedel <margoschaedel@gmail.com>
2018-08-14 16:17:45 -0600

    Update whale facts file

--- src/Facts.js
+++ src/Facts.js
@@ -1,44 +1,44 @@
 const Facts = [
   {
-    title: "Stegosaurus",
-    text: "Fossils of this genus date to the Late Jurassic period, where they are found in Kimmeridgian to early Tithonian aged strata, between 155 and 150 million years ago, in the western United States of America and Portugal."
+    title: "Blue Whale",
+    text: "The Blue Whale is the largest known mammal that has ever lived, with a length of up to 105ft long and weight of 150 tons. That’s the weight equivalent of 150 small cars!  It is larger than any of the dinosaurs. The biggest recorded blue whale was a female in the Antarctic Ocean that was 30.5 m long (more than 3.5 times the length of a double-decker bus and as long as a Boeing 737 plane) with an estimated weight of 144 tonnes (almost the same as 2,000 men). The tongue alone of a blue whale can weigh as much as an elephant and an entire football team could stand on it! The heart of a blue whale is about the size of a VW Beetle car and weighs up to 450kg. The aorta, a major blood vessel for the heart, is big enough for a human child to crawl through."
   },
   {
-    title: "Velociraptor",
-    text: "However, whereas most theropods had feet with three digits contacting the ground, dromaeosaurids like Velociraptor walked on only their third and fourth digits. The second digit, for which Velociraptor is most famous, was highly modified and held retracted off the ground. It bore a relatively large, sickle-shaped claw, typical of dromaeosaurid and troodontid dinosaurs. This enlarged claw, which could grow to over 6.5 cm long around its outer edge, was most likely a predatory device used to tear into or restrain struggling prey."
+    title: "Humpback Whale",
+    text: "Humpback whales are easy to tell apart by the markings on the underside of their tail fins. Each one has a unique pattern on his or her tail. Humpback whales only eat in summer, feeding off their fat reserves during the winter."
   },
   {
-    title: "Triceratops",
-    text: "The first named specimen now attributed to Triceratops is a pair of brow horns attached to a skull roof, found near Denver, Colorado in the spring of 1887."
+    title: "Orca (Killer Whale)",
+    text: "Orcas, also known as killer whales, are toothed whales and though they may have the word “whale” in their name, for classification purposes, they are actually considered dolphins. They are highly social and composed of matrilineal family groups. Orcas have long, rounded bodies with large dorsal fins at the middle of their backs. Their black bodies are marked with white patches on the underside and near the eyes. They are some of the top predators in the marine world. They feed mostly on fish, but some hunt seals, sea lions, walruses, and other whales."
   },
   {
-    title: "Spinosaurus",
-    text: "Spinosaurus is known to have eaten fish, and most scientists believe that it hunted both terrestrial and aquatic prey; evidence suggests that it lived both on land and in water as a modern crocodilian does."
+    title: "Sperm Whale",
+    text: "Sperm whales are champion divers. Adults can stay underwater for almost two hours and dive to depths of 2,000 metres or more. They eat squid, which can live very deep in the ocean, so sperm whales have to dive down into the deepest parts of the sea to catch them. The sperm whale’s huge head, which is up to a third of its overall body length, houses the heaviest brain in the animal kingdom - up to 9kg. The head also consists of a cavity large enough to park a car inside that contains a yellowish wax called spermaceti that was much sought after by whalers."
   },
   {
-    title: "Brachiosaurus",
-    text: "Brachiosaurus was a quadrupedal animal with a small skull, a long neck, a large trunk with a high-ellipsoid cross section, a long, muscular tail and slender, columnar limbs. Large air sacs connected to the lung system were present in the neck and trunk, invading the vertebrae and ribs, greatly reducing the overall density."
+    title: "Beluga Whale",
+    text: "Belugas are sometimes referred to as 'melonheads' due to the bulbous structure that occupies the whale’s forehead. The fatty organ is believed to aid in echolocation (a process in which animals use echoes of their calls to locate and identify objects), and it can be observed changing shape during whale vocalizations. Another nickname belugas have earned for themselves is 'sea canary' due to the rich and varied vocal range of the beluga. Scientists have documented at least 11 distinct beluga whale sounds, including high-pitched whistles, clicks, mews, bleats, chirps, and bell-like tones. Despite being a 'toothed whale', beluga whales do not chew their food; instead they swallow their prey whole. The beluga is able to swim backwards. Belugas are highly social creatures and generally live together in small groups known as pods. These vocal communicators are often found in the Arctic Ocean’s coastal waters travelling, foraging and socializing in both small and large pods."
   },
   {
-    title: "Masosaurus",
-    text: "The first remains known to science were a fragmentary skull from a chalk quarry in the St Pietersberg, a hill near Maastricht, the Netherlands, found in 1764 and collected by lieutenant Jean Baptiste Drouin in 1766. It considered it to be a species of \"big breathing fish\" (Pisces cetacei, in other words: a whale)."
+    title: "Grey Whale",
+    text: "Grey whales migrate huge distances and some may even rival the humpback for distance travelled. Some travel a round-trip of between 16,000–20,000 km (10,000–12,400 miles) every year between their winter calving lagoons in the warm waters of Mexico and their summer feeding grounds in the cold Arctic seas. However, a female grey whale has recently been recorded as having made an even longer round-trip of 22,500km (14,000 miles) migrating between the east coast of Russia and the breeding grounds of Mexico. To put this into perspective, the continent of Africa is approximately 8,000 km (5,000 miles) from north to south. In its lifetime – that’s about 40 years – a grey whale travels a distance that is equivalent to going to the moon and back!"
   },
   {
-    title: "Allosaurus",
-    text: "Allosaurus was a large bipedal predator. Its skull was large and equipped with dozens of sharp, serrated teeth. It averaged 8.5 m (28 ft) in length, though fragmentary remains suggest it could have reached over 12 m (39 ft). Relative to the large and powerful hindlimbs, its three-fingered forelimbs were small, and the body was balanced by a long and heavily muscled tail."
+    title: "Narwhal",
+    text: "Narwhals are distinguishable by the 2-3m long tusk that is found mostly on males. In Europe, these tusks were once sold as the horns of the mythical unicorn. Ongoing trade in their 'ivory' combined with other factors has led to regional declines over recent decades. In the summer months they are found to congregate in the hundreds and even thousands on rich feeding grounds, whilst during winter the majority of them disperse into smaller groups. The narwhal and the beluga together comprise the Monodontidae family."
   },
   {
-    title: "Brontosaurus",
-    text: "Brontosaurus had long, thin necks and small heads, adapted for a herbivorous lifestyle; a bulky, heavy torso; and long, whip-like tails. The various species lived during the Late Jurassic epoch in the Morrison Formation of North America, going extinct by the end of the Jurassic."
+    title: "Bowhead Whale",
+    text: "The bowhead whale has the longest baleen of any whale with the plates reaching 5m in length (the blue whale's baleen measures just one metre). The second species to become the primary target of commercial whaling, and hunted to near extinction in the middle of the 20th century, four of the five remaining stocks of bowhead whales remain Vulnerable, Endangered or Critically Endangered. Found only in Arctic and sub-Arctic regions, it would be difficult to confuse the bowhead whale with any other large whale as few range this far north except the occasional humpback or grey whale. The distinctive double-humped profile of the bowhead whale at the surface and lack of dorsal fin allows it to be distinguished from any rare visitors. Discoveries of two stone harpoon points from the late 19th century in the skull of a whale killed by Inuits in 1993 prove that the bowhead whale can live to be over 200 years of age – one of the longest-lived mammals yet known to science."
   },
   {
-    title: "Dilophosaurus",
-    text: "Dilophosaurus was featured in the novel Jurassic Park and its movie adaptation, wherein it was given the fictional abilities to spit venom and expand a cowl on its neck, as well as being smaller than the real animal. It was designated as the state dinosaur of Connecticut in 2017."
+    title: "Fin Whale",
+    text: "The fin whale is the world's second largest animal after the blue whale. They can dive to depths of 230 metres and make low-frequency noises (infrasound) which can be as loud as 188 decibels. These sounds cannot be heard by humans, but can be detected by other fin whales up to 850 km away. An individual fin whale is identified by the pattern of light-coloured chevrons on its back as well as by the size and shape of its dorsal fin. Records of females have been found with multiple foetuses, but it is unlikely that more than one would survive. The oldest specimen captured in Antarctica was 111 years old. Fin whales have also been known to mate with blue whales and to produce first generation offspring. Despite their size, the fin whale is known as the 'greyhound of the sea' and can reach speeds of up to 20mph (32kph)!"
   },
   {
-    title: "Pteranodon",
-    text: "They lived during the late Cretaceous geological period of North America in present-day Kansas, Alabama, Nebraska, Wyoming, and South Dakota. More fossil specimens of Pteranodon have been found than any other pterosaur, with about 1,200 specimens known to science, many of them well preserved with nearly complete skulls and articulated skeletons."
+    title: "Southern Right Whale",
+    text: "Right whales are so named because whalers considered them the 'right whale' to catch, being easy to approach, swimming slowly, living close to shore and floating when dead to make access to their valuable oil, meat and whalebone simpler. As a result between 1805 and 1844 alone, about 45,000 right whales were killed and they came very close to extinction. Although protected since 1937, only the southern right whale is showing signs of recovery. Southern right whales have many outstanding characteristics - the most notable perhaps being that they have the largest testes in the animal kingdom - with each pair weighing a tonne. They also have a large head, which is about a quarter of their body length with a strongly arched mouth."
   }
 ]
 
-export default Facts
+export default Facts;
